057951434748e7e5f8492c4a2bca0092bc95b28e
Daniel Waters <daniel.waters@softwire.com>
2021-12-15 16:10:47 +0000

    a few attempts at optimising, none successful

--- day15/main.rb
+++ day15/main.rb
@@ -11,7 +11,8 @@ Coord = Struct.new(:x, :y) do
   end
 
   def in_grid?(min_x, max_x, min_y, max_y)
-    self.x.between?(min_x, max_x) && self.y.between?(min_y, max_y)
+    0 <= self.x && self.x <= max_x && 0 <= self.y && self.y <= max_y
+    #self.x.between?(min_x, max_x) && self.y.between?(min_y, max_y)
   end
 end
 
@@ -34,7 +35,7 @@ min_risks[starting_corner] = 0
 points_to_search = starting_corner.neighbours
 
 while min_risks.values.any?(&:nil?) do
- point_set = next_set & chitons.keys
+ point_set = points_to_search & chitons.keys
  next_set = []
  point_set.each do |point|
    min_risks[point] ||= chitons[point] + point.neighbours.map { |neighbour| min_risks[neighbour] }.compact.min
@@ -52,6 +53,8 @@ puts min_risks[Coord.new(max_x, max_y)]
 
 larger_map = {}
 
+max_x = chitons.keys.map(&:x).max
+max_y = chitons.keys.map(&:y).max
 x_repeat = max_x + 1
 y_repeat = max_y + 1
 
@@ -106,4 +109,49 @@ puts (Time.now - start)
 
 start = Time.now
 puts search(larger_map)
+puts (Time.now - start)
+
+# optimising
+
+Search = Struct.new(:point, :value) do
+  def <=>(other)
+    self.value <=> other.value
+  end
+end
+
+def pqueue_search(map)
+  lowest_risk_by_point = {}
+
+  starting_corner = Search.new(Coord.new(0, 0), 0)
+  lowest_risk_by_point[starting_corner.point] = 0
+  points_to_search = [starting_corner]
+
+  max_x = map.keys.map(&:x).max
+  max_y = map.keys.map(&:y).max
+  final_corner = Coord.new(max_x, max_y)
+
+  until points_to_search.empty?
+    search = points_to_search.shift
+    point = search.point
+
+    cost = search.value
+    point.neighbours.each do |neighbour|
+      if neighbour.in_grid?(0, max_x, 0, max_y)
+        neighbour_cost = cost + map[neighbour]
+        previous_cost = lowest_risk_by_point[neighbour]
+        if previous_cost.nil? || previous_cost > neighbour_cost
+          lowest_risk_by_point[neighbour] = neighbour_cost
+          index = points_to_search.bsearch_index{|p| lowest_risk_by_point[p.point] > neighbour_cost} || points_to_search.size
+          points_to_search.insert(index, Search.new(neighbour, neighbour_cost))
+        end
+      end
+    end
+  end
+
+  lowest_risk_by_point[final_corner]
+end
+
+
+start = Time.now
+puts pqueue_search(larger_map)
 puts (Time.now - start)
