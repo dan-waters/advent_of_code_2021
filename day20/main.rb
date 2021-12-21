4d323d27b7a7887da2df8cf48822b16a78a30263
Daniel Waters <daniel.waters@softwire.com>
2021-12-21 11:38:35 +0000

    some performance tweaks for day 20

--- day20/main.rb
+++ day20/main.rb
@@ -2,17 +2,31 @@ require 'open-uri'
 
 Coordinate = Struct.new(:x, :y) do
   def neighbours
-    [
-      Coordinate.new(x - 1, y - 1),
-      Coordinate.new(x, y - 1),
-      Coordinate.new(x + 1, y - 1),
-      Coordinate.new(x - 1, y),
+    @neighbours ||= [
+      find_coord(x - 1, y - 1),
+      find_coord(x, y - 1),
+      find_coord(x + 1, y - 1),
+      find_coord(x - 1, y),
       self,
-      Coordinate.new(x + 1, y),
-      Coordinate.new(x - 1, y + 1),
-      Coordinate.new(x, y + 1),
-      Coordinate.new(x + 1, y + 1)
+      find_coord(x + 1, y),
+      find_coord(x - 1, y + 1),
+      find_coord(x, y + 1),
+      find_coord(x + 1, y + 1)
     ]
+  end
+
+  def hash
+    @hash ||= super
+  end
+end
+
+COORDS = {}
+
+def find_coord(x, y)
+  if COORDS[[x, y]]
+    COORDS[[x, y]]
+  else
+    COORDS[[x, y]] = Coordinate.new(x, y)
   end
 end
 
@@ -21,7 +35,7 @@ image = {}
 
 open('image.txt').readlines(chomp: true).each_with_index do |line, y|
   line.chars.each_with_index do |char, x|
-    coord = Coordinate.new(x, y)
+    coord = find_coord(x, y)
     image[coord] = (char == '#' ? 1 : 0)
   end
 end
@@ -33,7 +47,7 @@ end
     coord.neighbours.each do |neighbour|
       unless image[neighbour]
         image[neighbour] = i % 2
-        processed_image[neighbour] = (i+1) % 2
+        processed_image[neighbour] = (i + 1) % 2
       end
       value <<= 1
       value |= image[neighbour]
@@ -43,7 +57,7 @@ end
   image = processed_image
 end
 
-puts image.values.select{|i| i == 1}.count
+puts image.values.select { |i| i == 1 }.count
 
 # part 2
 
@@ -52,30 +66,32 @@ image = {}
 
 open('image.txt').readlines(chomp: true).each_with_index do |line, y|
   line.chars.each_with_index do |char, x|
-    coord = Coordinate.new(x, y)
+    coord = find_coord(x, y)
     image[coord] = (char == '#' ? 1 : 0)
   end
 end
 
+now = Time.now
 50.times do |i|
   processed_image = {}
   image.keys.each do |coord|
     value = 0
     coord.neighbours.each do |neighbour|
-      unless image[neighbour]
+      unless neighbour_val = image[neighbour]
         # by inspecting the algorithm, we can see that the current value
         # of the background is going to be off-on-off-...
         # and the next colour will be on-off-on...
         # this lets us dynamically expand the grid
-        image[neighbour] = i % 2
-        processed_image[neighbour] = (i+1) % 2
+        neighbour_val = image[neighbour] = i % 2
+        processed_image[neighbour] = (i + 1) % 2
       end
       value <<= 1
-      value |= image[neighbour]
+      value |= neighbour_val
     end
     processed_image[coord] = (algorithm[value] == '#' ? 1 : 0)
   end
   image = processed_image
 end
 
-puts image.values.select{|i| i == 1}.count
+puts image.values.select { |i| i == 1 }.count
+puts (Time.now - now)
